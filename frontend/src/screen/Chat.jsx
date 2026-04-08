7faa2ae8e7b6183ba42f796d7eaa372a268129ed
Karan300666 <karan3072005@gmail.com>
2026-04-08 16:04:59 +0530

    Fix API endpoint URLs in Chat.jsx

--- frontend/src/screen/Chat.jsx
+++ frontend/src/screen/Chat.jsx
@@ -64,7 +64,7 @@ const Chat = () => {
   }
   
   async function addCollaborators(){
-    axios.put('https://live-chat-app-0vfn.onrender.com//api/project/add-users' , {
+    axios.put('https://live-chat-app-0vfn.onrender.com/api/project/add-users' , {
       projectId : location.state.project._id ,
       users : Array.from(selectedUserIds)
     } ,{
@@ -105,7 +105,7 @@ useEffect(() => {
 function presentUsersInProject(){
   userPannelRef.current.style.left = "0%"
   
-  axios.get('https://live-chat-app-0vfn.onrender.com//api/project/users-project', {
+  axios.get('https://live-chat-app-0vfn.onrender.com/api/project/users-project', {
            params: {
             projectId: location.state.project._id
          },
